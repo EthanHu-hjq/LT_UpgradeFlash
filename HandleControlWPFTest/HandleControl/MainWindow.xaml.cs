40af215a488888a3b678212937dd1da85b95303c
ethan.hu <Ethan.Hu@tymphany.com>
2025-04-17 08:02:54 +0800

    first push code

---
+++ HandleControlWPFTest/HandleControl/MainWindow.xaml.cs
@@ -0,0 +1,430 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using System.Reflection.Metadata;
+
+namespace HandleControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region 引入user32.dll中的EnumChildWindows函数 已拷贝
+        [DllImport("user32.dll")]
+        static extern int EnumChildWindows(IntPtr hWndParent, EnumChildProc lpEnumFunc, IntPtr lParam);
+
+        //窗口控件类名获取
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetWindowText(IntPtr hWnd,StringBuilder caption, int count);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetDlgCtrlID(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool SetWindowText(IntPtr hWnd, string lpString);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("user32.dll",CharSet = CharSet.Auto)]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        static extern int IsWindowEnabled(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SetFocus(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, string lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, StringBuilder lParam);
+
+
+        // 定义消息常量1
+        const int WM_SETTEXT = 0x000C;
+
+        #endregion
+
+        public delegate bool EnumChildProc(IntPtr hwnd, int lParam);//1
+
+        //声明一个返回值为Dictionary<IntPtr, string>的方法GetControlContents，有一个IntPtr类型的参数hwnd
+        public Dictionary<IntPtr, string> GetControlContents(IntPtr hwnd)
+        {
+            var controlContents = new Dictionary<IntPtr, string>();
+            //枚举所有子控件
+            return controlContents;
+        }
+
+        //存储所有控件句柄的集合1
+        List<IntPtr> controlHandles = new List<IntPtr>();
+
+        //枚举回调函数1
+        private bool EnumChildCallback(IntPtr hwnd, int lParam)
+        {
+            controlHandles.Add(hwnd);
+            return true; // 继续枚举
+        }
+
+        //定义exe路径1
+        static string rootPath =@"D:\Project\Tymphany\HDMI Board_2025-04-15\HDMI Board\LT Upgrade Flash\Upgrade_Flash_For_Application.exe";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region 设置Pro按键状态的方法1
+        //设置Pro按键状态
+        const int BM_SETCHECK = 0x00F1; // 设置选中状态
+        const int BM_CHECKED = 0x0001; // 选中
+        const int BM_UNCHECKED = 0x0000; // 未选中
+        const int BM_CLICK = 0x00F5;//模拟按钮点击的消息
+
+        //通过句柄想按钮发送BM_SETCHECK消息来设置按钮的选中状态1
+        public void SetButtonCheck(IntPtr hwnd, bool isChecked)
+        {
+            int checkState = isChecked ? BM_CHECKED : BM_UNCHECKED;
+            SendMessage(hwnd, BM_SETCHECK, (IntPtr)checkState, IntPtr.Zero);
+        }
+        //通过句柄想按钮发送BM_CLICK消息来模拟按钮点击1
+        public void ClickButton(IntPtr hwnd)
+        {
+            PostMessage(hwnd, BM_CLICK, IntPtr.Zero, IntPtr.Zero);
+        }
+        #endregion
+
+        #region 初始化Chip控件的方法11
+        //向Chip控件添加内容消息
+        const int CB_ADDSTRING = 0x0143; // 添加字符串消息1
+        const int CB_SELECTSTRING = 0x014D; // 选择字符串消息1
+
+        //加载XML并初始化ComboBox控件Chip
+        public void InitializeComboBox(IntPtr comboBoxHandle,string xmlFilePath,string defaultSelect)
+        {
+            //加载XML文件
+            XDocument xmlDoc = XDocument.Load(xmlFilePath);
+
+            //遍历所有ChipType节点
+            foreach(var chipType in xmlDoc.Descendants("ChipType"))
+            {
+                string chipName = chipType.Attribute("ChipName")?.Value;
+                if(!string.IsNullOrEmpty(chipName))
+                {
+                    //向ComboBox控件添加字符串
+                    SendMessage(comboBoxHandle, CB_ADDSTRING, IntPtr.Zero, Marshal.StringToHGlobalAuto(chipName));
+                }
+            }
+            //默认选中指定的内容
+            if(!string.IsNullOrEmpty(defaultSelect))
+            {
+                //选择字符串
+                SendMessage(comboBoxHandle, CB_SELECTSTRING, IntPtr.Zero, Marshal.StringToHGlobalAuto(defaultSelect));
+            }
+        }
+        #endregion
+
+        IntPtr mainHnadle = IntPtr.Zero; // 主窗口句柄11
+        /// <summary>
+        /// 打开exe
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            #region 打开exe
+            Process process = new Process();
+            process.StartInfo.FileName = rootPath;
+            process.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(rootPath);//设置工作目录
+            process.StartInfo.UseShellExecute = true;
+            process.Start();
+            #endregion
+
+            Thread.Sleep(3000);
+            #region 获取窗口句柄
+            process.WaitForInputIdle(); // 等待进程进入空闲状态
+            IntPtr hWnd = process.MainWindowHandle; // 获取主窗口句柄
+            if(hWnd == IntPtr.Zero)
+            {
+                Thread.Sleep(1000); // 等待1秒
+                hWnd = process.MainWindowHandle; // 再次获取主窗口句柄
+            }
+            //将句柄显示到UI的TextBlock上
+            if (hWnd != IntPtr.Zero)
+            {
+                string handle = hWnd.ToString("X");
+                this.showhandle.Text = "Handle: " + handle;
+            }
+            else
+            {
+                this.showhandle.Text = "Handle: Not Found";
+            }
+            mainHnadle = hWnd; // 保存主窗口句柄
+            #endregion
+
+            #region 获取handle信息
+            EnumChildWindows(hWnd, EnumChildCallback, 0); // 枚举子窗口
+            for (int i = 0; i < controlHandles.Count; i++)
+            {
+                IntPtr handle = controlHandles[i]; // 获取句柄
+                StringBuilder className = new StringBuilder(256);
+                StringBuilder text = new StringBuilder(256); // 创建StringBuilder对象用于存储类名和标题
+
+                GetClassName(handle, className, className.Capacity); // 获取类名
+                int controlID = GetDlgCtrlID(handle); // 获取控件ID
+                if (controlID == 1006)
+                {
+                    SendMessage(handle,WM_SETTEXT,0, @"0001"); // 设置控件标题
+                }
+
+                string handleStr = controlHandles[i].ToString("X");
+                string controlType = className.ToString(); // 获取控件类型
+
+                #region 获取控件caption
+                const int maxLength = 256;
+                StringBuilder caption = new StringBuilder(maxLength);
+                int lenghth = GetWindowText(handle, caption, maxLength); // 获取控件标题
+                #endregion
+
+                this.itemsControl.Items.Add($"Handle: {handleStr}__Type: {controlType}__caption: {caption}__ControlID:{controlID}"); // 将句柄添加到ItemsControl
+                
+                ////初始化ComboBox控件Chip
+                //if(controlType == "ComboBox")
+                //{
+                //    //设置Chip控件的XML文件路径
+                //    string xmlFilePath = @"D:\Project\Tymphany\HDMI Board_2025-04-15\HDMI Board\LT Upgrade Flash\UpgradeFlash.xml";
+                //    string defaultSelection = "LT6911(UX/UXB/UXC)";
+                //    InitializeComboBox(handle, xmlFilePath,defaultSelection); // 初始化ComboBox控件
+                //}
+            }
+            #endregion
+
+            //初始化Chip
+            IntPtr comboBoxHandle = FindWindowEx(mainHnadle, IntPtr.Zero, "ComboBox", null);
+            //设置Chip控件的XML文件路径
+            string xmlFilePath = @"D:\Project\Tymphany\HDMI Board_2025-04-15\HDMI Board\LT Upgrade Flash\UpgradeFlash.xml";
+            string defaultSelection = "LT6911(UX/UXB/UXC)";
+            InitializeComboBox(comboBoxHandle, xmlFilePath, defaultSelection); // 初始化ComboBox控件
+
+        }
+
+        /// <summary>
+        /// 关闭exe
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Process[] processes = Process.GetProcessesByName("Upgrade_Flash_For_Application");
+            foreach (Process process in processes)
+            {
+                try
+                {
+                    process.Kill();
+                    process.WaitForExit();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void HandleFileDialog(string defaultFilePath)
+        {
+            // 循环等待文件选择框出现
+            IntPtr fileDialogHandle = IntPtr.Zero;
+            Thread.Sleep(4);
+            for (int i = 0; i < 10; i++) // 尝试 10 次，每次间隔 500ms
+            {
+                fileDialogHandle = FindWindowEx(IntPtr.Zero, IntPtr.Zero, "#32770", null); // "#32770" 是文件选择框的类名
+                if (fileDialogHandle != IntPtr.Zero)
+                {
+                    break;
+                }
+                Thread.Sleep(500); // 等待 500ms
+            }
+
+            if (fileDialogHandle != IntPtr.Zero)
+            {
+                // 获取文件选择框中的文件路径输入框句柄
+                //IntPtr editHandle = FindWindowEx(fileDialogHandle, IntPtr.Zero, "Edit", null);
+                IntPtr editHandle = FindEditControl(fileDialogHandle);
+                if (editHandle != IntPtr.Zero)
+                {
+                    // 设置默认路径和文件名
+                    SetWindowText(editHandle, defaultFilePath);
+                }
+
+                // 获取“打开”按钮的句柄
+                IntPtr openButtonHandle = FindWindowEx(fileDialogHandle, IntPtr.Zero, "Button", "打开(&O)");
+                if (openButtonHandle != IntPtr.Zero)
+                {
+                    // 模拟点击“打开”按钮
+                    ClickButton(openButtonHandle);
+                }
+            }
+            else
+            {
+                MessageBox.Show("未找到文件选择框！");
+            }
+        }
+        private void FocusOnControl(IntPtr mainWindowHandle, IntPtr controlHandle)
+        {
+            // 将主窗口置于前台
+            if (SetForegroundWindow(mainWindowHandle))
+            {
+                // 设置焦点到指定控件
+                SetFocus(controlHandle);
+            }
+            else
+            {
+                MessageBox.Show("无法将窗口置于前台！");
+            }
+        }
+
+        /// <summary>
+        /// 查找文件选择框中的编辑控件
+        /// </summary>
+        /// <param name="parentHandle"></param>
+        /// <returns></returns>
+        private IntPtr FindEditControl(IntPtr parentHandle)
+        {
+            IntPtr editHandle = IntPtr.Zero;
+            EnumChildWindows(parentHandle, (hwnd, lParam) =>
+            {
+                StringBuilder className = new StringBuilder(256);
+                GetClassName(hwnd, className, className.Capacity);
+                if (className.ToString() == "Edit")
+                {
+                    editHandle = hwnd;
+                    return false; // 停止枚举
+                }
+                return true; // 继续枚举
+            }, IntPtr.Zero);
+            return editHandle;
+        }
+        /// <summary>
+        /// 模拟点击Prog按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            foreach (var handle in controlHandles)
+            {
+                int controlID = GetDlgCtrlID(handle); // 获取控件ID
+                if (controlID == 1004)
+                {
+                    //FocusOnControl(mainHnadle, handle);
+                    //SetWindowText(handle, "00001"); // 设置控件标题
+                    bool isEnabled = IsWindowEnabled(handle) != 0;
+                    if (!isEnabled)
+                    {
+                        MessageBox.Show("控件未启用！");
+                    }
+                    else
+                    {
+                        //SetWindowText(handle, @"D:\Project\Tymphany\HDMI Board_2025-04-15\HDMI Board\LT Upgrade Flash\text.hex");
+                        SendMessage(handle,WM_SETTEXT,0, @"D:\Project\Tymphany\HDMI Board_2025-04-15\HDMI Board\LT Upgrade Flash\text.hex");
+                    }
+                    //const int maxLength = 256;
+                    //StringBuilder caption = new StringBuilder(maxLength);
+                    //int lenghth = GetWindowText(handle, caption, maxLength); // 获取控件标题
+                    
+                }
+                //StringBuilder className = new StringBuilder(256);
+                //GetClassName(handle, className, className.Capacity); // 获取类名
+
+                //string handleStr = handle.ToString("X");
+                //string controlType = className.ToString(); // 获取控件类型
+
+                //#region 获取控件caption
+                //const int maxLength = 256;
+                //StringBuilder caption = new StringBuilder(maxLength);
+                //int lenghth = GetWindowText(handle, caption, maxLength); // 获取控件标题
+                //#endregion
+
+                //if (caption.ToString() == "Prog")
+                //{
+                //    ClickButton(handle); // 模拟点击
+
+                //    // 启动新线程处理文件选择弹框
+                //    // 启动新线程处理文件选择框
+                //    Task.Run(() =>
+                //    {
+                //        string defaultFilePath = @"D:\Project\Tymphany\HDMI Board_2025-04-15\HDMI Board\LT Upgrade Flash\text.hex";
+                //        HandleFileDialog(defaultFilePath);
+                //    });
+                //}
+            }
+        }
+
+        const int WM_GETTEXT = 0x000D; // 获取文本的消息
+        const int WM_GETTEXTLENGTH = 0x000E; // 获取文本长度的消息
+
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            foreach (var handle in controlHandles)
+            {
+                int controlID = GetDlgCtrlID(handle); // 获取控件ID
+                if (controlID == 1010)
+                {
+                    StringBuilder sb = null;
+                    // 获取文本框内容的长度
+                    int textLength = SendMessage(handle, WM_GETTEXTLENGTH, 0, sb);
+                    if (textLength > 0)
+                    {
+                        // 创建一个StringBuilder对象来存储文本内容
+                        StringBuilder textContent = new StringBuilder(textLength + 1);
+                        // 获取文本框内容
+                        SendMessage(handle, WM_GETTEXT, textLength + 1, textContent);
+                        // 将文本内容显示在UI的TextBlock上
+                        this.Log.Text = textContent.ToString();
+                        break;
+                    }
+                    else
+                    {
+                        this.Log.Text = "TextBox is empty.";
+                    }
+                }
+            }
+        }
+
+        //修改Chip控件的选中项
+        private void SelectChip(IntPtr comboBoxHandle, string chipName)
+        {
+            //选择字符串
+            SendMessage(comboBoxHandle, CB_SELECTSTRING, 0, Marshal.StringToHGlobalAuto(chipName));
+        }
+
+        //选择芯片型号
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            IntPtr comboBoxHandle = FindWindowEx(mainHnadle, IntPtr.Zero, "ComboBox", null);
+            SendMessage(comboBoxHandle, CB_SELECTSTRING, IntPtr.Zero, Marshal.StringToHGlobalAuto("LT86102UXE/LT8612UX"));
+            //foreach (var handle in controlHandles)
+            //{
+            //    StringBuilder className = new StringBuilder(256);
+            //    GetClassName(handle, className, className.Capacity); // 获取类名
+            //    string controlType = className.ToString(); // 获取控件类型
+            //    if (controlType == "ComboBox")
+            //    {
+            //        SelectChip(handle, "LT9611(UX/UXC)"); // 选择芯片型号
+            //        break;
+            //    }
+            //}
+        }
+    }
+}
---
+++ LT_UpgradeFlash_Lib/LT_UpgradeFlash.cs
@@ -0,0 +1,249 @@
+﻿//LT OCM芯片烧写工具DLL
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LT_UpgradeFlash_Lib
+{
+    public class LT_UpgradeFlash
+    {
+        public delegate bool EnumChildProc(IntPtr hwnd, int lParam);//定义枚举子句柄委托
+
+        #region 引入user32.dll
+        [DllImport("user32.dll")]
+        static extern int EnumChildWindows(IntPtr hWndParent, EnumChildProc lpEnumFunc, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder caption, int count);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int GetDlgCtrlID(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool SetWindowText(IntPtr hWnd, string lpString);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        static extern int IsWindowEnabled(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SetFocus(IntPtr hWnd);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, string lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, StringBuilder lParam);
+        #endregion
+
+        #region 定义消息常量
+        const int WM_SETTEXT = 0x000C;//设置文本消息
+        const int BM_CLICK = 0x00F5;//模拟按钮点击的消息
+        const int BM_SETCHECK = 0x00F1; // 设置按钮选中状态
+        const int BM_CHECKED = 0x0001; // 按钮选中
+        const int BM_UNCHECKED = 0x0000; // 按钮未选中
+        const int CB_ADDSTRING = 0x0143; // ComboBox添加字符串消息
+        const int CB_SELECTSTRING = 0x014D; // ComboBox选择字符串消息
+        const int WM_GETTEXT = 0x000D; // 获取文本的消息
+        const int WM_GETTEXTLENGTH = 0x000E; // 获取文本长度的消息
+        #endregion
+
+        #region 定义变量
+        List<IntPtr> controlHandles = new List<IntPtr>();//存储控件句柄的集合
+        static string rootPath = null;//烧录软件exe路径
+        IntPtr mainHnadle = IntPtr.Zero; // 主窗口句柄
+        #endregion
+
+        
+        #region 声明子方法
+        /// <summary>
+        /// 枚举控件句柄
+        /// </summary>
+        /// <param name="hwnd">控件句柄</param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private bool EnumChildCallback(IntPtr hwnd, int lParam)
+        {
+            controlHandles.Add(hwnd);
+            return true; // 继续枚举
+        }
+        /// <summary>
+        /// 模拟外部进程按钮点击
+        /// </summary>
+        /// <param name="hwnd">按钮句柄</param>
+        private void ClickButton(IntPtr hwnd)
+        {
+            PostMessage(hwnd, BM_CLICK, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        /// <summary>
+        /// 加载XML并初始化ComboBox控件Chip
+        /// </summary>
+        /// <param name="comboBoxHandle">控件句柄</param>
+        /// <param name="xmlFilePath">xml文件路径</param>
+        /// <param name="defaultSelect">下拉框默认选择</param>
+        private void InitializeChip(IntPtr comboBoxHandle, string xmlFilePath, string defaultSelect)
+        {
+            //加载XML文件
+            XDocument xmlDoc = XDocument.Load(xmlFilePath);
+
+            //遍历所有ChipType节点
+            foreach (var chipType in xmlDoc.Descendants("ChipType"))
+            {
+                string chipName = chipType.Attribute("ChipName")?.Value;
+                if (!string.IsNullOrEmpty(chipName))
+                {
+                    //向ComboBox控件添加字符串
+                    SendMessage(comboBoxHandle, CB_ADDSTRING, IntPtr.Zero, Marshal.StringToHGlobalAuto(chipName));
+                }
+            }
+            //默认选中指定的内容
+            if (!string.IsNullOrEmpty(defaultSelect))
+            {
+                //选择字符串
+                SendMessage(comboBoxHandle, CB_SELECTSTRING, IntPtr.Zero, Marshal.StringToHGlobalAuto(defaultSelect));
+            }
+        }
+
+        #endregion
+
+
+        #region 声明调用方法
+
+        #region 打开烧录软件exe
+        /// <summary>
+        /// 打开烧录软件exe
+        /// </summary>
+        /// <param name="exePath">exe路径</param>
+        /// <param name="waitForOpenDone">等待exe打开完成时间</param>
+        public void OpenUpgradeFlash(string exePath, int waitForOpenDone,string xmlFilePath,string chipSelection)
+        {
+            rootPath = exePath;
+            //启动烧录软件exe
+            Process process = new Process();
+            process.StartInfo.FileName = rootPath;
+            process.StartInfo.WorkingDirectory = System.IO.Path.GetDirectoryName(rootPath);//设置工作目录
+            process.StartInfo.UseShellExecute = true;
+            process.Start();
+
+            //等待进程加载完成
+            Thread.Sleep(waitForOpenDone);
+            process.WaitForInputIdle(); // 等待进程进入空闲状态
+
+            //获取主窗口句柄
+            mainHnadle = process.MainWindowHandle;
+            //如果主窗口句柄为空，则等待一段时间再次获取
+            if(mainHnadle == IntPtr.Zero)
+            {
+                Thread.Sleep(1000);
+                mainHnadle = process.MainWindowHandle;
+            }
+
+            //获取所有子控件句柄
+            EnumChildWindows(mainHnadle,EnumChildCallback, IntPtr.Zero);
+
+            //初始化Chip控件
+            IntPtr comboBoxHandle = FindWindowEx(mainHnadle, IntPtr.Zero, "ComboBox", null);
+            InitializeChip(comboBoxHandle, xmlFilePath, chipSelection);//加载XML并初始化ComboBox控件Chip
+        }
+        #endregion
+
+        
+
+        #region 选择芯片型号
+        public void SelectChip(string chipName)
+        {
+            IntPtr comboBoxHandle = FindWindowEx(mainHnadle, IntPtr.Zero, "ComboBox", null);
+            SendMessage(comboBoxHandle, CB_SELECTSTRING, IntPtr.Zero, Marshal.StringToHGlobalAuto(chipName));
+        }
+        #endregion
+
+        #region 烧录
+        /// <summary>
+        /// 烧录方法
+        /// </summary>
+        /// <param name="hexFilePath">烧录文件</param>
+        public string Prog(string hexFilePath,int waitForDone)
+        {
+            foreach(var handle in controlHandles)
+            {
+                int ctrlId = GetDlgCtrlID(handle);//获取控件ID
+                if(ctrlId == 1004) //判断控件是否为加载HEX文件的控件
+                {
+                    SendMessage(handle, WM_SETTEXT, 0, hexFilePath);//设置HEX文件路径
+                }
+                if(ctrlId == 1001)//判断控件是否为烧录按钮
+                {
+                    //设置焦点
+                    //SetForegroundWindow(handle);
+                    //SetFocus(handle);
+                    //模拟点击
+                    ClickButton(handle);
+                    break;
+                }
+            }
+            //等待烧录完成
+            Thread.Sleep(waitForDone);//等待烧录完成
+            string result = ReadLog();//读取烧录结果
+
+
+            return "";
+        }
+        #endregion
+
+        #region 读取烧录结果
+        
+        #endregion
+
+
+        #region 读取Log记录
+        public string ReadLog()
+        {
+            StringBuilder result = new StringBuilder(256);
+            foreach (var handle in controlHandles)
+            {
+                int ctrlId = GetDlgCtrlID(handle);//获取控件ID
+                if (ctrlId == 1010) //判断控件是否为结果显示控件
+                {
+                    StringBuilder textContent = null;
+                    //获取文本长度
+                    int length = SendMessage(handle, WM_GETTEXTLENGTH, 0, textContent);
+                    if(length > 0)
+                    {
+                        //获取文本内容
+                        textContent = new StringBuilder(length + 1);
+                        //获取文本内容
+                        SendMessage(handle, WM_GETTEXT, textContent.Capacity, textContent);
+                        result.Append(textContent.ToString());
+                    }
+                    break;
+                }
+            }
+            return result.ToString();
+        }
+        #endregion
+
+        #region 关闭烧录软件exe
+        public void CloseUpgradeFlash()
+        {
+            //关闭烧录软件exe
+            Process[] processes = Process.GetProcessesByName(System.IO.Path.GetFileNameWithoutExtension(rootPath));
+            foreach (Process process in processes)
+            {
+                process.Kill();
+            }
+        }
+        #endregion
+
+
+        #endregion
+    }
+}
